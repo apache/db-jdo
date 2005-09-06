fca99d672b06e0aa657a041374284d5ff4c2a1d7
Michelle Caisse <mcaisse@apache.org>
2005-09-06 20:42:51 +0000

    Remove unused file from repository

--- tck20/test/sql/derby/createdb.sh
+++
@@ -1,9 +0,0 @@
-# Edit DBPATH and BASEPATH for your installation
-export DBPATH="c:/derby"  # where the derby db and log go
-export BASEPATH="c:/svnrepos/jdo/trunk/tck20" # your tck20 install dir
-
-
-export CLASSPATH="$BASEPATH/iut_jars/derbytools.jar;$BASEPATH/iut_jars/derby.jar"
-rm -rf $DBPATH/jdotckdb
-java -Dderby.system.home=$DBPATH org.apache.derby.tools.ij schema1.sql
-#java -Dderby.system.home=$DBPATH org.apache.derby.tools.ij schema2.sql
