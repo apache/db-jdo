38466e146bdc42b77457a1937bfc43474d5a7f87
Michelle Caisse <mcaisse@apache.org>
2006-09-19 22:29:56 +0000

    JDO-193 Add files for mapping a List field without a join table.  Also, fix a problem in jdo metadata for companyMapWithoutJoin.

---
+++ tck20/src/java/org/apache/jdo/tck/pc/companyListWithoutJoin/CompanyFactoryConcreteClass.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright 2005 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.jdo.tck.pc.companyListWithoutJoin;
+
+import java.math.BigDecimal;
+
+import java.util.Date;
+
+import javax.jdo.PersistenceManager;
+
+/*
+ * This class is the company factory that uses constructors of the
+ * concrete classes.
+ */
+public  class CompanyFactoryConcreteClass implements CompanyFactory {
+
+    /** */
+    public static final Class[] tearDownClasses = new Class[] {
+        PartTimeEmployee.class, FullTimeEmployee.class,  
+        Department.class, Company.class
+    };
+
+    public CompanyFactoryConcreteClass(PersistenceManager pm) {
+    }
+
+    public CompanyFactoryConcreteClass() {
+    }
+
+    public ICompany newCompany(long companyid, 
+            String name, Date founded) {
+        return new Company(companyid, name, founded);
+    }
+
+    public IDepartment newDepartment(long deptid, String name) {
+        return new Department(deptid, name);
+    }
+
+    public IDepartment newDepartment(long deptid, 
+            String name, ICompany company) {
+        return new Department(deptid, name, company);
+    }
+
+    public IFullTimeEmployee newFullTimeEmployee(long personid, 
+            String first, String last, String middle, 
+            Date born, Date hired, double sal) {
+        return new FullTimeEmployee(personid, first, last, middle,
+                born, hired, sal);
+    }
+
+    public IPartTimeEmployee newPartTimeEmployee(long personid, 
+            String first, String last, String middle, 
+            Date born, Date hired, double wage) {
+        return new PartTimeEmployee(personid, first, last, middle, 
+                born, hired, wage);
+    } 
+
+    public IPerson newPerson(long personid, 
+            String firstname, String lastname, String middlename, 
+            Date birthdate) {
+        return new Person(personid, firstname, lastname, middlename,
+                birthdate);
+    }
+
+    public Class[] getTearDownClasses() {
+        return tearDownClasses;
+    }
+}
---
+++ tck20/src/java/org/apache/jdo/tck/pc/companyListWithoutJoin/IPerson.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2005 The Apache Software Foundation.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at 
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License.
+ */
+ 
+package org.apache.jdo.tck.pc.companyListWithoutJoin;
+
+import java.util.Date;
+
+/**
+ * This interface represents the persistent state of Person.
+ * Javadoc was deliberately omitted because it would distract from
+ * the purpose of the interface.
+ */
+public interface IPerson {
+
+    long getPersonid();
+    String getLastname();
+    String getFirstname();
+    String getMiddlename();
+    Date getBirthdate();
+    
+    void setPersonid(long personid);
+    void setLastname(String lastname);
+    void setFirstname(String firstname);
+    void setMiddlename(String middlename);
+    void setBirthdate(Date birthdate);
+
+}
