85b250d2e47b136d826a9ccb7c9db7dc52a96195
Michelle Caisse <mcaisse@apache.org>
2007-07-22 16:23:06 +0000

    JDO-503 Rename conf files; add factory classes, move some factories from company to companyFactoryAnnotatedFC

---
+++ tck2/src/java/org/apache/jdo/tck/pc/companyAnnotatedFC/CompanyFactoryAnnotatedFCConcreteDelegator.java
@@ -0,0 +1,44 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.jdo.tck.pc.companyAnnotatedFC;
+
+import javax.jdo.PersistenceManager;
+import org.apache.jdo.tck.pc.company.CompanyFactoryAnnotatedAbstractDelegator;
+
+/*
+ * CompanyFactoryAnnotatedPMFieldClass.java
+ *
+ * This class uses the PersistenceManager.newInstance method with the concrete
+ * class as a parameter.
+ */
+public class CompanyFactoryAnnotatedFCConcreteDelegator
+        extends CompanyFactoryAnnotatedAbstractDelegator {
+    
+    /**
+     * Creates a new instance of CompanyFactory
+     */
+    public CompanyFactoryAnnotatedFCConcreteDelegator(PersistenceManager pm) {
+        super(pm);
+        if (isAppIdentity){
+            delegate = new CompanyFactoryAnnotatedFCAppConcrete(pm);
+        } else { //datastoreidentity
+            delegate = new CompanyFactoryAnnotatedFCDSConcrete(pm);
+        }
+    }
+    
+}
