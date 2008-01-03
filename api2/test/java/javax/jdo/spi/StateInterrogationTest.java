357092afbb584dc2e672c56e9b916662fd6db11a
Matthew T. Adams <madams@apache.org>
2008-01-03 05:45:54 +0000

    Rolling back to version 545369

--- api2/test/java/javax/jdo/spi/StateInterrogationTest.java
+++ api2/test/java/javax/jdo/spi/StateInterrogationTest.java
@@ -17,12 +17,14 @@
 
 package javax.jdo.spi;
 
-import javax.jdo.JDOHelper;
-import javax.jdo.PersistenceManager;
-import javax.jdo.util.AbstractTest;
-import javax.jdo.util.BatchTestRunner;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Proxy;
+
+import javax.jdo.JDOHelper;
+import javax.jdo.PersistenceManager;
+
+import javax.jdo.util.AbstractTest;
+import javax.jdo.util.BatchTestRunner;
 
 /**
  * This class tests the StateInterrogation interface. The test is in
