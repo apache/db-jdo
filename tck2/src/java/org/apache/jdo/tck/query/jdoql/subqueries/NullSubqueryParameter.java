e3ee10da17ba37ecd659bacd3e3a68a7600f4e9c
Andy Jefferson <andyj@apache.org>
2008-01-12 20:57:41 +0000

    assignments are illegal in JDOQL where clause

--- tck2/src/java/org/apache/jdo/tck/query/jdoql/subqueries/NullSubqueryParameter.java
+++ tck2/src/java/org/apache/jdo/tck/query/jdoql/subqueries/NullSubqueryParameter.java
@@ -63,12 +63,12 @@ public class NullSubqueryParameter extends SubqueriesTest {
             new String[]{"emp1", "emp2", "emp4", "emp6", "emp7", "emp10"});
 
         String singleStringJDOQL = 
-            "SELECT FROM Employee WHERE this.weeklyhours = emp.weeklyhours && " +
-            "emp.firstname = 'emp1First' VARIABLES Employee emp";
+            "SELECT FROM Employee WHERE this.weeklyhours == emp.weeklyhours && " +
+            "emp.firstname == 'emp1First' VARIABLES Employee emp";
 
         // API query
         Query apiQuery = pm.newQuery(Employee.class);
-        apiQuery.setFilter("this.weeklyhours > emp.weeklyhours && emp.firstname = 'emp1First'");
+        apiQuery.setFilter("this.weeklyhours > emp.weeklyhours && emp.firstname == 'emp1First'");
         // null subquery parameter
         apiQuery.addSubquery(null, "Employee emp", null); 
         executeJDOQuery(ASSERTION_FAILED, apiQuery, singleStringJDOQL, 
