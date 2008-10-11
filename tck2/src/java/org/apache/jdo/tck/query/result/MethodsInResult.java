f4cebb108580b4c99960172f1865af832d5de7e0
Andy Jefferson <andyj@apache.org>
2008-10-11 14:56:31 +0000

    Package incorrect - wouldn't compile in Eclipse like that

--- tck2/src/java/org/apache/jdo/tck/query/result/MethodsInResult.java
+++ tck2/src/java/org/apache/jdo/tck/query/result/MethodsInResult.java
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
  
-package org.apache.jdo.tck.query.jdoql.result;
+package org.apache.jdo.tck.query.result;
 
 import java.math.BigInteger;
 import java.util.Arrays;
