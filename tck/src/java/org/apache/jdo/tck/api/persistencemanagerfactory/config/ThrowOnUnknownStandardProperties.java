bf53be289f8bd4bf7ef1193d1e5fba408c9f6264
Andy Jefferson <andyj@apache.org>
2012-02-17 19:29:12 +0000

    JDO-696 Added fix to use of reflection call of method when expecting an exception to be thrown - will be the cause of an InvocationTargetException

--- tck/src/java/org/apache/jdo/tck/api/persistencemanagerfactory/config/ThrowOnUnknownStandardProperties.java
+++ tck/src/java/org/apache/jdo/tck/api/persistencemanagerfactory/config/ThrowOnUnknownStandardProperties.java
@@ -1,5 +1,22 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License.
+ */
 package org.apache.jdo.tck.api.persistencemanagerfactory.config;
 
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 import java.util.Map;
@@ -67,8 +84,13 @@ public class ThrowOnUnknownStandardProperties extends JDO_Test {
             invokeGetPMF(p);
             fail("testUnknownStandardProperty should result in JDOUserException. "
                     + "No exception was thrown.");
-        } catch (JDOUserException x) {
-            // :)
+        } catch (InvocationTargetException ite) {
+            Throwable cause = ite.getCause();
+            if (cause != null && cause instanceof JDOUserException) {
+            	// :)
+            } else {
+                throw new JDOException("PMF threw " + cause + " instead of JDOException");
+            }
         } catch (Exception e) {
             throw new JDOException(
                     "failed to invoke static getPersistenceManagerFactory(Map) method on PMF class",
@@ -87,8 +109,13 @@ public class ThrowOnUnknownStandardProperties extends JDO_Test {
             invokeGetPMF(p);
             fail("testUnknownStandardProperties should result in JDOUserException. "
                     + "No exception was thrown.");
-        } catch (JDOUserException thrown) {
-            x = thrown;
+        } catch (InvocationTargetException ite) {
+            Throwable cause = ite.getCause();
+            if (cause != null && cause instanceof JDOUserException) {
+                x = (JDOUserException) cause;
+            } else {
+                throw new JDOException("PMF threw " + cause + " instead of JDOException");
+            }
         } catch (Exception e) {
             throw new JDOException(
                     "failed to invoke static getPersistenceManagerFactory(Map) method on PMF class",
