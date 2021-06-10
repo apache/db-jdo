bddffaba0384d1393b9091461ad9eaba0d9848b8
Michael Bouschen <mbo@apache.org>
2021-06-10 21:09:00 +0200

    JDO-709: remove ElementType.TYPE from @Target

--- api/src/main/java/javax/jdo/annotations/Convert.java
+++ api/src/main/java/javax/jdo/annotations/Convert.java
@@ -17,7 +17,6 @@
 package javax.jdo.annotations;
 
 import java.lang.annotation.ElementType;
-import java.lang.annotation.Repeatable;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
@@ -39,7 +38,7 @@ import javax.jdo.AttributeConverter;
  * @since 3.2
  */
 @Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.TYPE, ElementType.METHOD, ElementType.FIELD})
+@Target({ElementType.METHOD, ElementType.FIELD})
 public @interface Convert {
 
 	/**
