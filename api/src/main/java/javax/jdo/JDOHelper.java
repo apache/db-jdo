9093c7778be868b48056a6d4dad1a92c1bfb9f80
Michael Bouschen <Michael.Bouschen@akquinet.de>
2023-01-01 17:09:00 +0100

    JDO-823: A conditionally executed single line should be denoted by indentation

--- api/src/main/java/javax/jdo/JDOHelper.java
+++ api/src/main/java/javax/jdo/JDOHelper.java
@@ -1740,9 +1740,14 @@ public class JDOHelper {
           (PrivilegedExceptionAction<Object>) () -> method.invoke(instance, parameters));
     } catch (PrivilegedActionException ex) {
       Exception cause = ex.getException();
-      if (cause instanceof IllegalAccessException) throw (IllegalAccessException) cause;
-      else // if (cause instanceof InvocationTargetException)
-      throw (InvocationTargetException) cause;
+      if (cause instanceof IllegalAccessException) {
+        throw (IllegalAccessException) cause;
+      } else if (cause instanceof InvocationTargetException) {
+        throw (InvocationTargetException) cause;
+      }
+      else {
+        throw new RuntimeException(cause);
+      }
     }
   }
 
