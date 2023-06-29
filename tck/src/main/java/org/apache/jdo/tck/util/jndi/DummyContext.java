c7094b0b176eb3765a876560770b76391f5642b0
Tilmann <zoodb@gmx.de>
2023-06-29 18:07:20 +0200

    lint

--- tck/src/main/java/org/apache/jdo/tck/util/jndi/DummyContext.java
+++ tck/src/main/java/org/apache/jdo/tck/util/jndi/DummyContext.java
@@ -1,173 +1,166 @@
 package org.apache.jdo.tck.util.jndi;
 
-import javax.naming.*;
 import java.util.Hashtable;
+import javax.naming.*;
 
 public class DummyContext implements Context {
-    private final Hashtable<String, Object> map;
+  private final Hashtable<String, Object> map;
 
-    public DummyContext(Hashtable<String, Object> environment) {
-        map = environment;
+  public DummyContext(Hashtable<String, Object> environment) {
+    map = environment;
+  }
+
+  @Override
+  public Object lookup(Name name) throws NamingException {
+    throw new UnsupportedOperationException("lookup(name):" + name.toString());
+  }
+
+  @Override
+  public Object lookup(String name) throws NamingException {
+    // lookup: java:comp/BeanManager
+    if (!map.containsKey(name)) {
+      throw new NamingException("lookup: " + name);
     }
+    return map.get(name);
+  }
 
-    @Override
-    public Object lookup(Name name) throws NamingException {
-        throw new UnsupportedOperationException("lookup(name):" + name.toString());
+  @Override
+  public void bind(Name name, Object obj) throws NamingException {
+    throw new UnsupportedOperationException("bind(name):" + name.toString());
+  }
+
+  @Override
+  public void bind(String name, Object obj) throws NamingException {
+    if (map.containsKey(name)) {
+      throw new IllegalStateException("bind: " + name);
     }
+    map.put(name, obj);
+  }
 
-    @Override
-    public Object lookup(String name) throws NamingException {
-        // lookup: java:comp/BeanManager
-        if (!map.containsKey(name)) {
-            throw new NamingException("lookup: " + name);
-        }
-        return map.get(name);
+  @Override
+  public void rebind(Name name, Object obj) throws NamingException {
+    throw new UnsupportedOperationException("rebind(name):" + name.toString());
+  }
+
+  @Override
+  public void rebind(String name, Object obj) throws NamingException {
+    if (!map.containsKey(name)) {
+      throw new IllegalStateException("rebind: " + name);
     }
+    map.put(name, obj);
+  }
 
-    @Override
-    public void bind(Name name, Object obj) throws NamingException {
-        throw new UnsupportedOperationException("bind(name):" + name.toString());
-    }
+  @Override
+  public void unbind(Name name) throws NamingException {
+    throw new UnsupportedOperationException("unbind(name):" + name.toString());
+  }
 
-    @Override
-    public void bind(String name, Object obj) throws NamingException {
-        if (map.containsKey(name)) {
-            throw new IllegalStateException("bind: " + name);
-        }
-        map.put(name, obj);
-    }
+  @Override
+  public void unbind(String name) throws NamingException {
+    // unbind: C:\work\github\db-jdo\tck\target\classes\pmf.ser
+    map.remove(name);
+  }
 
-    @Override
-    public void rebind(Name name, Object obj) throws NamingException {
-        throw new UnsupportedOperationException("rebind(name):" + name.toString());
+  @Override
+  public void rename(Name oldName, Name newName) throws NamingException {
+    throw new UnsupportedOperationException("unbind(name):" + oldName.toString());
+  }
 
-    }
+  @Override
+  public void rename(String oldName, String newName) throws NamingException {
+    throw new UnsupportedOperationException("unbind(name):" + oldName);
+  }
 
-    @Override
-    public void rebind(String name, Object obj) throws NamingException {
-        if (!map.containsKey(name)) {
-            throw new IllegalStateException("rebind: " + name);
-        }
-        map.put(name, obj);
-    }
+  @Override
+  public NamingEnumeration<NameClassPair> list(Name name) throws NamingException {
+    throw new UnsupportedOperationException("unbind(name):" + name.toString());
+  }
 
-    @Override
-    public void unbind(Name name) throws NamingException {
-        throw new UnsupportedOperationException("unbind(name):" + name.toString());
-    }
+  @Override
+  public NamingEnumeration<NameClassPair> list(String name) throws NamingException {
+    throw new UnsupportedOperationException("unbind(name):" + name);
+  }
 
-    @Override
-    public void unbind(String name) throws NamingException {
-        // unbind: C:\work\github\db-jdo\tck\target\classes\pmf.ser
-        map.remove(name);
-    }
+  @Override
+  public NamingEnumeration<Binding> listBindings(Name name) throws NamingException {
+    throw new UnsupportedOperationException("unbind(name):" + name.toString());
+  }
 
-    @Override
-    public void rename(Name oldName, Name newName) throws NamingException {
-        throw new UnsupportedOperationException("unbind(name):" + oldName.toString());
+  @Override
+  public NamingEnumeration<Binding> listBindings(String name) throws NamingException {
+    throw new UnsupportedOperationException("unbind(name):" + name);
+  }
 
-    }
+  @Override
+  public void destroySubcontext(Name name) throws NamingException {
+    throw new UnsupportedOperationException("unbind(name):" + name.toString());
+  }
 
-    @Override
-    public void rename(String oldName, String newName) throws NamingException {
-        throw new UnsupportedOperationException("unbind(name):" + oldName);
+  @Override
+  public void destroySubcontext(String name) throws NamingException {
+    throw new UnsupportedOperationException("unbind(name):" + name);
+  }
 
-    }
+  @Override
+  public Context createSubcontext(Name name) throws NamingException {
+    throw new UnsupportedOperationException("unbind(name):" + name.toString());
+  }
 
-    @Override
-    public NamingEnumeration<NameClassPair> list(Name name) throws NamingException {
-        throw new UnsupportedOperationException("unbind(name):" + name.toString());
-    }
+  @Override
+  public Context createSubcontext(String name) throws NamingException {
+    throw new UnsupportedOperationException("unbind(name):" + name);
+  }
 
-    @Override
-    public NamingEnumeration<NameClassPair> list(String name) throws NamingException {
-        throw new UnsupportedOperationException("unbind(name):" + name);
-    }
+  @Override
+  public Object lookupLink(Name name) throws NamingException {
+    throw new UnsupportedOperationException("unbind(name):" + name.toString());
+  }
 
-    @Override
-    public NamingEnumeration<Binding> listBindings(Name name) throws NamingException {
-        throw new UnsupportedOperationException("unbind(name):" + name.toString());
-    }
+  @Override
+  public Object lookupLink(String name) throws NamingException {
+    throw new UnsupportedOperationException("unbind(name):" + name);
+  }
 
-    @Override
-    public NamingEnumeration<Binding> listBindings(String name) throws NamingException {
-        throw new UnsupportedOperationException("unbind(name):" + name);
-    }
+  @Override
+  public NameParser getNameParser(Name name) throws NamingException {
+    throw new UnsupportedOperationException("unbind(name):" + name.toString());
+  }
 
-    @Override
-    public void destroySubcontext(Name name) throws NamingException {
-        throw new UnsupportedOperationException("unbind(name):" + name.toString());
+  @Override
+  public NameParser getNameParser(String name) throws NamingException {
+    throw new UnsupportedOperationException("unbind(name):" + name);
+  }
 
-    }
+  @Override
+  public Name composeName(Name name, Name prefix) throws NamingException {
+    throw new UnsupportedOperationException("unbind(name):" + name.toString());
+  }
 
-    @Override
-    public void destroySubcontext(String name) throws NamingException {
-        throw new UnsupportedOperationException("unbind(name):" + name);
+  @Override
+  public String composeName(String name, String prefix) throws NamingException {
+    throw new UnsupportedOperationException("unbind(name):" + name);
+  }
 
-    }
+  @Override
+  public Object addToEnvironment(String propName, Object propVal) throws NamingException {
+    throw new UnsupportedOperationException("unbind(name):" + propName);
+  }
 
-    @Override
-    public Context createSubcontext(Name name) throws NamingException {
-        throw new UnsupportedOperationException("unbind(name):" + name.toString());
-    }
+  @Override
+  public Object removeFromEnvironment(String propName) throws NamingException {
+    throw new UnsupportedOperationException("unbind(name):" + propName);
+  }
 
-    @Override
-    public Context createSubcontext(String name) throws NamingException {
-        throw new UnsupportedOperationException("unbind(name):" + name);
-    }
+  @Override
+  public Hashtable<?, ?> getEnvironment() throws NamingException {
+    throw new UnsupportedOperationException("getEnvironment()");
+  }
 
-    @Override
-    public Object lookupLink(Name name) throws NamingException {
-        throw new UnsupportedOperationException("unbind(name):" + name.toString());
-    }
+  @Override
+  public void close() throws NamingException {}
 
-    @Override
-    public Object lookupLink(String name) throws NamingException {
-        throw new UnsupportedOperationException("unbind(name):" + name);
-    }
-
-    @Override
-    public NameParser getNameParser(Name name) throws NamingException {
-        throw new UnsupportedOperationException("unbind(name):" + name.toString());
-    }
-
-    @Override
-    public NameParser getNameParser(String name) throws NamingException {
-        throw new UnsupportedOperationException("unbind(name):" + name);
-    }
-
-    @Override
-    public Name composeName(Name name, Name prefix) throws NamingException {
-        throw new UnsupportedOperationException("unbind(name):" + name.toString());
-    }
-
-    @Override
-    public String composeName(String name, String prefix) throws NamingException {
-        throw new UnsupportedOperationException("unbind(name):" + name);
-    }
-
-    @Override
-    public Object addToEnvironment(String propName, Object propVal) throws NamingException {
-        throw new UnsupportedOperationException("unbind(name):" + propName);
-    }
-
-    @Override
-    public Object removeFromEnvironment(String propName) throws NamingException {
-        throw new UnsupportedOperationException("unbind(name):" + propName);
-    }
-
-    @Override
-    public Hashtable<?, ?> getEnvironment() throws NamingException {
-        throw new UnsupportedOperationException("getEnvironment()");
-    }
-
-    @Override
-    public void close() throws NamingException {
-
-    }
-
-    @Override
-    public String getNameInNamespace() throws NamingException {
-        throw new UnsupportedOperationException("getNameInNamespace()");
-    }
+  @Override
+  public String getNameInNamespace() throws NamingException {
+    throw new UnsupportedOperationException("getNameInNamespace()");
+  }
 }
--- tck/src/main/java/org/apache/jdo/tck/util/jndi/DummyContextFactory.java
+++ tck/src/main/java/org/apache/jdo/tck/util/jndi/DummyContextFactory.java
@@ -1,16 +1,16 @@
 package org.apache.jdo.tck.util.jndi;
 
+import java.util.Hashtable;
 import javax.naming.Context;
 import javax.naming.NamingException;
 import javax.naming.spi.InitialContextFactory;
-import java.util.Hashtable;
 
 public class DummyContextFactory implements InitialContextFactory {
 
-    @Override
-    public Context getInitialContext(Hashtable<?, ?> environment) throws NamingException {
-        // jndiName: C:\work\github\db-jdo\tck\target\classes\pmf.ser
-        // e: java.naming.factory.initial -> org.apache.jdo.tck.util.jndi.DummyContextFactory
-        return new DummyContext((Hashtable<String, Object>) environment);
-    }
+  @Override
+  public Context getInitialContext(Hashtable<?, ?> environment) throws NamingException {
+    // jndiName: C:\work\github\db-jdo\tck\target\classes\pmf.ser
+    // e: java.naming.factory.initial -> org.apache.jdo.tck.util.jndi.DummyContextFactory
+    return new DummyContext((Hashtable<String, Object>) environment);
+  }
 }
