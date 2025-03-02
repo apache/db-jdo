1ea30bc9ba513c8f7c8b5a58408c71e22374c0d8
Michael Bouschen <Michael.Bouschen@akquinet.de>
2025-03-02 16:45:47 +0100

    JDO-850: Add SampleModifyQuery test method using typesafe query API

--- tck/src/main/java/org/apache/jdo/tck/query/api/SampleModifyQueries.java
+++ tck/src/main/java/org/apache/jdo/tck/query/api/SampleModifyQueries.java
@@ -16,11 +16,16 @@
  */
 package org.apache.jdo.tck.query.api;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import javax.jdo.JDOQLTypedQuery;
 import javax.jdo.Query;
 import javax.jdo.Transaction;
+import javax.jdo.query.NumericExpression;
 import org.apache.jdo.tck.pc.company.CompanyModelReader;
 import org.apache.jdo.tck.pc.company.FullTimeEmployee;
+import org.apache.jdo.tck.pc.company.QFullTimeEmployee;
 import org.apache.jdo.tck.query.QueryTest;
 import org.junit.jupiter.api.Test;
 
@@ -30,6 +35,17 @@ import org.junit.jupiter.api.Test;
  * <B>Assertion IDs:</B> <br>
  * <B>Assertion Description: </B> This test class runs the example modify queries from the JDO
  * specification.
+ *
+ * <p>There are up to four test methods per test case:
+ *
+ * <ul>
+ *   <li>testQueryxxa: runtime constructed JDO query using deletePersistentAll taking parameters to
+ *       run the query
+ *   <li>testQueryxxb: runtime constructed JDO query using setNamedParameters to specify the
+ *       parameter values and no-arg deletePersistentAll to run the query
+ *   <li>testQueryxxc: runtime constructed JDO query using setParameters to specify the parameter
+ *       values and no-arg deletePersistentAll to run the query
+ *   <li>testQueryxxf: JDOQLTypedQuery version
  */
 public class SampleModifyQueries extends QueryTest {
 
@@ -46,13 +62,125 @@ public class SampleModifyQueries extends QueryTest {
    * <p>This query deletes all Employees who make more than the parameter salary.
    */
   @Test
-  public void testQuery21() {
+  public void testQuery21a() {
     Transaction tx = pm.currentTransaction();
     try {
       tx.begin();
       Query<FullTimeEmployee> q = pm.newQuery(FullTimeEmployee.class, "salary > sal");
       q.declareParameters("Double sal");
       q.deletePersistentAll(30000.);
+      tx.commit();
+
+      tx.begin();
+      Query<FullTimeEmployee> allQuery = pm.newQuery(FullTimeEmployee.class);
+      List<FullTimeEmployee> allFTE = allQuery.executeList();
+      if (!allFTE.isEmpty()) {
+        fail(
+            ASSERTION_FAILED,
+            "All FullTimeEmployee instances should have been deleted,"
+                + " there are still "
+                + allFTE.size()
+                + " instances left.");
+      }
+      tx.commit();
+    } finally {
+      if (tx.isActive()) {
+        tx.rollback();
+      }
+    }
+  }
+
+  /**
+   * Deleting Multiple Instances.
+   *
+   * <p>This query deletes all Employees who make more than the parameter salary.
+   */
+  @Test
+  public void testQuery21b() {
+    Transaction tx = pm.currentTransaction();
+    try {
+      tx.begin();
+      Query<FullTimeEmployee> q = pm.newQuery(FullTimeEmployee.class, "salary > sal");
+      q.declareParameters("Double sal");
+      Map<String, Object> paramValues = new HashMap<>();
+      paramValues.put("sal", 30000.);
+      q.setNamedParameters(paramValues);
+      q.deletePersistentAll();
+      tx.commit();
+
+      tx.begin();
+      Query<FullTimeEmployee> allQuery = pm.newQuery(FullTimeEmployee.class);
+      List<FullTimeEmployee> allFTE = allQuery.executeList();
+      if (!allFTE.isEmpty()) {
+        fail(
+            ASSERTION_FAILED,
+            "All FullTimeEmployee instances should have been deleted,"
+                + " there are still "
+                + allFTE.size()
+                + " instances left.");
+      }
+      tx.commit();
+    } finally {
+      if (tx.isActive()) {
+        tx.rollback();
+      }
+    }
+  }
+
+  /**
+   * Deleting Multiple Instances.
+   *
+   * <p>This query deletes all Employees who make more than the parameter salary.
+   */
+  @Test
+  public void testQuery21c() {
+    Transaction tx = pm.currentTransaction();
+    try {
+      tx.begin();
+      Query<FullTimeEmployee> q = pm.newQuery(FullTimeEmployee.class, "salary > sal");
+      q.declareParameters("Double sal");
+      q.setParameters(30000.);
+      q.deletePersistentAll();
+      tx.commit();
+
+      tx.begin();
+      Query<FullTimeEmployee> allQuery = pm.newQuery(FullTimeEmployee.class);
+      List<FullTimeEmployee> allFTE = allQuery.executeList();
+      if (!allFTE.isEmpty()) {
+        fail(
+            ASSERTION_FAILED,
+            "All FullTimeEmployee instances should have been deleted,"
+                + " there are still "
+                + allFTE.size()
+                + " instances left.");
+      }
+      tx.commit();
+    } finally {
+      if (tx.isActive()) {
+        tx.rollback();
+      }
+    }
+  }
+
+  /**
+   * Deleting Multiple Instances.
+   *
+   * <p>This query deletes all Employees who make more than the parameter salary.
+   */
+  @Test
+  public void testQuery21f() {
+    Transaction tx = pm.currentTransaction();
+    try {
+      tx.begin();
+      try (JDOQLTypedQuery<FullTimeEmployee> q = pm.newJDOQLTypedQuery(FullTimeEmployee.class)) {
+        QFullTimeEmployee cand = QFullTimeEmployee.candidate("this");
+        NumericExpression<Double> sal = q.numericParameter("sal", Double.class);
+        q.filter(cand.salary.gt(sal));
+        q.setParameter("sal", 30000.);
+        q.deletePersistentAll();
+      } catch (Exception ex) {
+        fail(ASSERTION_FAILED, ex.getLocalizedMessage());
+      }
       tx.commit();
 
       tx.begin();
