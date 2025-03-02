b434ad8d167b52798e6ec82fccc61f389484acfb
Tilmann <zoodb@gmx.de>
2025-03-02 19:29:46 +0100

    Fix for result.Variable see JDO-851

--- tck/src/main/java/org/apache/jdo/tck/pc/order/DefaultListableInstanceFactory.java
+++ tck/src/main/java/org/apache/jdo/tck/pc/order/DefaultListableInstanceFactory.java
@@ -1,29 +1,32 @@
 package org.apache.jdo.tck.pc.order;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 
 public class DefaultListableInstanceFactory {
 
-    private final HashMap<String, Object> rootMap = new HashMap<>(); // TODO concurrent?
+  private final HashMap<String, Object> rootMap = new HashMap<>();
+  private final List<Object> rootList = new ArrayList<>();
 
-    public synchronized Object getBean(String name) {
-        if (!rootMap.containsKey(name)) {
-            throw new IllegalArgumentException("Not found: " + name);
-        }
-        if (rootMap.get(name) == null) {
-            throw new IllegalArgumentException("Not found2: " + name);
-        }
-        Object o = rootMap.get(name);
-        System.err.println("Found: " +name + "  -->> " + o.getClass());
-        // throw new IllegalStateException("Found: " +name + "  -->> " + o.getClass());
-        return rootMap.get(name);
+  public synchronized Object getBean(String name) {
+    if ("root".equals(name)) {
+      return getRootList();
     }
+    return rootMap.get(name);
+  }
 
-    public <T> T getBean(String name, Class<T> clazz) {
-        return clazz.cast(getBean(name));
-    }
+  public <T> T getBean(String name, Class<T> clazz) {
+    return clazz.cast(getBean(name));
+  }
 
-    protected synchronized void register(String name, Object obj) {
-        rootMap.put(name, obj);
-    }
+  protected synchronized void register(String name, Object obj) {
+    rootMap.put(name, obj);
+    rootList.add(obj);
+  }
+
+  public synchronized List<Object> getRootList() {
+    return Collections.unmodifiableList(rootList);
+  }
 }
